edb3a8d26dbf018d0e2a6663d876ee122a1cc41a
Olasupo <olasupoodebiyi@yahoo.com>
2020-11-02 10:36:47 +0000

    Second Commit

--- app.js
+++ app.js
@@ -19,4 +19,22 @@ const io = socketio(server);
 
 io.on("connection", (socket) => {
   console.log("New user connected");
+
+  socket.username = "Anonymous";
+
+  socket.on("change_username", (data) => {
+    socket.username = data.username;
+  });
+
+  socket.on("new_message", (data) => {
+    console.log("new message");
+    io.sockets.emit("receive_message", {
+      message: data.message,
+      username: socket.username,
+    });
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", { username: socket.username });
+  });
 });
